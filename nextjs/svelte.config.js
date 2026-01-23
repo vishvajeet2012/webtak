9c49c6939da90b62d4d1f2a0c405c359228eca65
vishvajeet2012 <djritikshukla@gmail.com>
2026-01-23 17:28:42 +0530

    Fix Vercel adapter config with nodejs22.x runtime

--- nextjs/svelte.config.js
+++ nextjs/svelte.config.js
@@ -3,15 +3,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// Using Vercel adapter for deployment
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'nodejs22.x'
+		})
 	}
 };
 
 export default config;
+
 
